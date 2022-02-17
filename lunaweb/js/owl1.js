a362b8c69588b4acdf8ab4fd3a6ceed818bb11e2
Heinrich Hike <contact@hike.dev>
2022-02-17 11:10:50 +0800

    Add files via upload

---
+++ lunaweb/js/owl1.js
@@ -0,0 +1,11 @@
+var owl = $('.owl-horse');
+            owl.owlCarousel({
+               nav: false,
+                dots: false,
+                items: 3,
+                loop: true,
+                margin: 10,
+                autoplay: true,
+                autoplayTimeout: 6000,
+                autoplayHoverPause: false
+            });
---
+++ lunaweb/js/owl2.js
@@ -0,0 +1,12 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    nav: false,
+    dots: false,
+    items: 4,
+    touchDrag  : false,
+    mouseDrag  : false,
+    loop: true,
+    margin: 10,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+});
